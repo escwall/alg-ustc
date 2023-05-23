9eedb5befc2bafbb3784f8d1b10aa9a1ff55a8e3
escwall <lvjinyang@mail.ustc.edu.cn>
2023-05-23 23:18:27 +0800

    fix bug

--- src/matrixMuti.cpp
+++ src/matrixMuti.cpp
@@ -3,40 +3,55 @@
 
 using namespace std;
 
-int main() {
-    vector<int> mm;
-    while(!cin.eof()) {
-        int e;
-        cin >> e;
-        mm.emplace_back(e);
+void print(const vector<vector<int>>& s, int i, int j) {
+    if(i == j) {
+        cout << "A" << i+1;
     }
-    int size = mm.size();
-    vector<vector<int>> dp(size, vector<int>(size, 0)), s(dp);
-    for(int i = 2; i <= size; ++i) {
-        for(int j = 0; j < size - i + 1; ++j) {
-            int m = j + i - 1;
-            dp[j][m] = INT_MAX;
-            for(int k = j; k < m; ++k) {
-                int cost = dp[j][k] + dp[k+1][m] + mm[j] * mm[k] * mm[m];
-                if(cost < dp[j][m]) {
-                    dp[j][m] = cost;
-                    s[j][m] = k;
+    else {
+        cout << "(";
+        print(s, i, s[i][j]-1);
+        print(s, s[i][j], j);
+        cout << ")";
+    }
+}
+
+int main() {
+    vector<int> p;
+    int e;
+    while(cin >> e) {
+        p.emplace_back(e);
+        if('\n' == cin.get())
+            break;
+    }
+    int n = p.size() - 1;
+    vector<vector<int>> dp(n, vector<int>(n)), s(dp);
+    for(int len = 2; len <= n; ++len) {
+        for(int i = 0; i < n - len + 1; ++i) {
+            int k = i + len - 1;
+            dp[i][k] = INT_MAX;
+            for(int j = i; j < k; ++j) {
+                int cost = dp[i][j] + dp[j+1][k] + p[i] * p[j+1] * p[k+1];
+                if(cost < dp[i][k]) {
+                    dp[i][k] = cost;
+                    s[i][k] = j+1;
                 }
-            }
+            } 
         }
     }
-    for(int i = 0; i < size; ++i) {
-        for(int j = 0; j < size; ++j) {
+    cout << "-------代价矩阵--------" << endl;
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n; ++j) {
             cout << dp[i][j] << " ";
         }
         cout << endl;
     }
-    cout << "--------------------" << endl;
-    for(int i = 0; i < size; ++i) {
-        for(int j = 0; j < size; ++j) {
+    cout << "-------括号矩阵--------" << endl;
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n; ++j) {
             cout << s[i][j] << " ";
         }
         cout << endl;
     }
+    print(s, 0, 5);
     return 0;
 }
