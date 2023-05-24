147c14ae8af772b0fe8f10c8705a5c250ccd8736
escwall <lvjinyang@mail.ustc.edu.cn>
2023-05-24 22:54:00 +0800

    修改一些细节

--- src/lab3/RBTree_insert.cpp
+++ src/lab3/RBTree_insert.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 enum COLOR {RED, BLACK};
 
-using TNode = struct TNode
+using TNode = struct TNode //结点数据结构
 {
     int key;
     COLOR color;
@@ -27,19 +27,19 @@ using TNode = struct TNode
     }
 };
 
-class RBTree {
+class RBTree { //红黑树类
 private:
-    TNode* root;
-    ofstream lnr, nlr, lot;
+    TNode* root; //红黑树根结点
+    ofstream lnr, nlr, lot; //用于中序、先序、层序遍历的文件流
 
-    void leftRotation(TNode* x) {
-        TNode* y = x->right;
-        x->right = y->left;
+    void leftRotation(TNode* x) { //左旋
+        TNode* y = x->right; //x的右孩子结点变为当前子树的根
+        x->right = y->left; //x的右孩子变为y的左孩子
         if(y->left != nullptr) {
             y->left->parent = x;
         }
         y->parent = x->parent;
-        if(x->parent == nullptr) {
+        if(x->parent == nullptr) { //x为根结点的情况
             root = y;
             _root = root;
         }
@@ -48,11 +48,11 @@ private:
         }
         else 
             x->parent->right = y;
-        y->left = x;
+        y->left = x; //x变为y的左孩子
         x->parent = y;
     }
 
-    void rightRotation(TNode* x) {
+    void rightRotation(TNode* x) { //与左旋同理，左右相反即可
         TNode* y = x->left;
         x->left = y->right;
         if(y->right != nullptr) {
@@ -72,75 +72,61 @@ private:
         x->parent = y;
     }
 
-    void insertFixup(TNode* z) {
-        bool flag = false;
-        while (z->parent != nullptr && z->parent->color == RED) {
+    void insertFixup(TNode* z) { //插入操作后维护红黑树性质
+        while (z->parent != nullptr && z->parent->color == RED) { //z的父结点为红色，需要调整
             if(z->parent->parent)
             {    
-                if(z->parent == z->parent->parent->left) {
+                if(z->parent == z->parent->parent->left) { //父结点为祖父结点的左孩子
                     TNode* y = z->parent->parent->right;
-                    if(y && y->color == RED) {    
-                        // if(y->color == RED) {
+                    if(y && y->color == RED) {  //叔叔结点为红色，将其和父结点都涂黑，祖父结点涂红，对应case1的情况
                         z->parent->color = BLACK;
                         y->color = BLACK;
                         z->parent->parent->color = RED;
                         z = z->parent->parent;
                         cout << "case1 ";
-                        flag = true; 
-                        // } 
                     }
                     else {
-                        if(z == z->parent->right) {
+                        if(z == z->parent->right) { //z为父结点的右孩子，对应形状为'<'
                             z = z->parent;
-                            leftRotation(z);
+                            leftRotation(z); //将z的父结点左旋,调整形状为'/'
                             cout << "case2 ";
-                            flag = true; 
                         }
                         z->parent->color = BLACK;
                         z->parent->parent->color = RED;
-                        rightRotation(z->parent->parent);
+                        rightRotation(z->parent->parent); //右旋祖父结点
                         cout << "case3 ";
-                        flag = true; 
                     }
                     
                 }
-                else {
+                else { //父结点为祖父结点的右孩子，与上面逻辑类似，左右相反即可
                     TNode* y = z->parent->parent->left;
                     if(y && y->color == RED) {
-                        // if(y->color == RED) {
                         z->parent->color = BLACK;
                         y->color = BLACK;
                         z->parent->parent->color = RED;
                         z = z->parent->parent;
                         cout << "case4 ";
-                        flag = true; 
-                        // }
                     }
                     else {
-                        if(z == z->parent->left) {
+                        if(z == z->parent->left) { //z为父结点的左孩子，对应形状为'>' 
                             z = z->parent;
-                            rightRotation(z);
-                            cout << "case5 ";
-                            flag = true; 
+                            rightRotation(z); //将z的父结点右旋,调整形状为'\'
+                            cout << "case5 "; 
                         }
                         z->parent->color = BLACK;
                         z->parent->parent->color = RED;
-                        leftRotation(z->parent->parent);
+                        leftRotation(z->parent->parent); //左旋祖父结点
                         cout << "case6 ";
-                        flag = true; 
                     }
                 }
             }
         }
-        if(root->color == RED) {
+        if(root->color == RED) { //如果根结点为红色，将根结点涂黑，对应case7的情况
             root->color = BLACK;
             cout << "case7 ";
-            flag = true; 
         }
-        // if(!flag)
-        //     cout << "case0 ";
     }
-    ~RBTree() {
+    ~RBTree() { //析构函数
         lnr.clear();
         nlr.clear();
         lot.clear();
@@ -160,20 +146,20 @@ public:
         _root = root;
     }
 
-    void insert(int key) {
+    void insert(int key) { //向红黑树中插入值为key的结点
         TNode* z = new TNode(key); //待插入的新结点, 左右孩子以及颜色均已在构造函数初始化
         TNode* y = nullptr; //为待插入结点记录其父结点
         TNode* x = root; //寻找插入位置
 
         while(x != nullptr) {
             y = x;
-            if(z->key < x->key)
+            if(z->key < x->key) //待插入结点值小于当前结点值，左子树找合适位置
                 x = x->left;
-            else 
+            else //否则在右子树中找合适位置
                 x = x->right;
         }
         z->parent = y;
-        if(y == nullptr) {
+        if(y == nullptr) { //插入位置为根结点情况
             root = z;
             _root = root;
         }
@@ -181,10 +167,10 @@ public:
             y->left = z;
         else 
             y->right = z;
-        insertFixup(z);
+        insertFixup(z); //插入新结点后维护红黑树
     }
 
-    void LNR(const TNode* root) {
+    void LNR(const TNode* root) { //中序遍历
         if(root == nullptr) return;
         LNR(root->left);
         string color = (root->color == RED) ? "red" : "black";
@@ -192,7 +178,7 @@ public:
         LNR(root->right);
     }
 
-    void NLR(const TNode* root) {
+    void NLR(const TNode* root) { //先序遍历
         if(root == nullptr) return;
         string color = (root->color == RED) ? "red" : "black";
         nlr << root->key << ", " << color << endl;
@@ -200,7 +186,7 @@ public:
         NLR(root->right);
     }
 
-    void LOT(const TNode* root) {
+    void LOT(const TNode* root) { //层序遍历
         queue<TNode*> q;
         TNode* r = new TNode(root);
         q.push(r);
